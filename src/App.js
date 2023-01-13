d565affe49c908c1b0a9e671738da7c957af84ef
Jonathan Amaral <jonathansamael@live.com>
2023-01-13 15:30:26 -0300

    "🚧 build: create useState to show listFilms"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import React, {useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import './App.css'
 import Tmdb from './Tmdb'
+import MovieRow from './components/movieRow/MovieRow'
+import FeaturedMovie from './components/featuredMovie/FeaturedMovie'
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
   useEffect(() => {
     const loadAll = async () => {
+      // Pegando a lista de filmes
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      // Pegando o featured
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randonChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+      let chosen = originals[0].items.results[randonChosen]
+
+      console.log(list);
     }
 
     loadAll();
   }, []);
 
   return (
-    <>
-      Test de nova Branch
-      
-    </>
+    <div className='page'>
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData}/>
+      }
+
+
+      <section className='lists'>
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+
+      </section>
+    </div>
   )
 }
