a7c5bbccfc89fe48d54287ed54246f3c67f10c3f
Jonathan Amaral <jonathansamael@live.com>
2023-01-18 16:03:56 -0300

    "🎯 feat: moviment MovieRow + add loading screen"

--- src/components/movieRow/MovieRow.js
+++ src/components/movieRow/MovieRow.js
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import './MovieRow.css';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 export default ({ title, items }) => {
+    const [scrollX, setScrollX] = useState(0);
 
+    const handleLeftArrow = () => {
+        let moveLineToLeft = scrollX + Math.round(window.innerWidth / 2);
+        if(moveLineToLeft > 0) {
+            moveLineToLeft = 0
+        }
+        setScrollX(moveLineToLeft);
+    }
+
+    const handleRightArrow = () => {
+        let moveLineToRight = scrollX - Math.round(window.innerWidth / 2);
+        let listWidth = items.results.length * 150;
+        if ((window.innerWidth - listWidth) > moveLineToRight) {
+            moveLineToRight = (window.innerWidth - listWidth) - 60;
+        }
+        setScrollX(moveLineToRight);
+    }
 
     return (
-        <div className="movierow">
+        <div className="movieRow">
             <h2>{title}</h2>
-            <div className="movieRow--left">
-                <NavigateBeforeIcon />
+            <div className="movieRow--left" onClick={handleLeftArrow}>
+                <NavigateBeforeIcon style={{ fontSize: 50 }} />
             </div>
-            <div className="movieRow--right">
-                <NavigateNextIcon />
+            <div className="movieRow--right" onClick={handleRightArrow}>
+                <NavigateNextIcon style={{ fontSize: 50 }} />
             </div>
             <div className="movieRow--listarea">
-                <div className="movierow--list">
+                <div className="movieRow--list" style={{ 
+                    marginLeft: scrollX,
+                    width: items.results.length * 150
+                    }}>
                     {items.results.length > 0 && items.results.map((item, key) => (
-                        <div key={key} className="movierow--item">
+                        <div key={key} className="movieRow--item">
                             <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
 
                         </div>
