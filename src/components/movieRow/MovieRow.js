eed60cedf02a60d1cb9567fa30eeb889c4716938
Jonathan Amaral <jonathansamael@live.com>
2023-01-17 21:04:37 -0300

    "🚧 feat: intall new icon package from mui/material"

--- src/components/movieRow/MovieRow.js
+++ src/components/movieRow/MovieRow.js
@@ -1,5 +1,7 @@
 import React from "react";
-import './MovieRow.css'
+import './MovieRow.css';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 export default ({ title, items }) => {
 
@@ -7,6 +9,12 @@ export default ({ title, items }) => {
     return (
         <div className="movierow">
             <h2>{title}</h2>
+            <div className="movieRow--left">
+                <NavigateBeforeIcon />
+            </div>
+            <div className="movieRow--right">
+                <NavigateNextIcon />
+            </div>
             <div className="movieRow--listarea">
                 <div className="movierow--list">
                     {items.results.length > 0 && items.results.map((item, key) => (
